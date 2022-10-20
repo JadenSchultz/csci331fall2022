329b95cfe8095435e9482f6c7f96c8e8e4601d78
Jaden <spacemanjaden125@gmail.com>
2022-10-19 20:19:58 -0600

    Tweaked the code yet again

--- lamp2/php/add.php
+++ lamp2/php/add.php
@@ -71,7 +71,7 @@ $sql = "SELECT * FROM randuser2";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-echo "<table class = 'table table-hover'>";
+echo "<table class='table table-hover'>";
 echo "<thead>";
 echo "  <tr>";
 echo "    <th>First Name</th>";
@@ -97,20 +97,20 @@ echo "</ttable>";
   echo "0 results";
 }
 
-
-
 $conn->close();
-
 ?>
-
-    <br><br>
-    <button onclick="history.back()">Back</button>
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<br><br>
+<div class = "mybuttonwrap">
+<button class = "btn btn-primary btn-lg mybutton" onclick="history.back()">&lt;&lt;Back</button>
+</div>
+<br>
 
 <?php 
   include "footer.php";

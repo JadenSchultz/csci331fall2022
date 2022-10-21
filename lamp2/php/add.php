0f7729f3f6bbf765e3ec2e6781a624a596220a1b
Jaden <spacemanjaden125@gmail.com>
2022-10-21 17:18:00 -0600

    Trying to fix indents again

--- lamp2/php/add.php
+++ lamp2/php/add.php
@@ -85,7 +85,7 @@ echo "<tbody>";
   while($row = $result->fetch_assoc()) {
     echo "  <tr>";
     echo "    <td>" . $row['firstname'] . "<td>";
-    echo "    <td>" . $row['lastname'] . "<td>";
+    echo "" . $row['lastname'] . "<td>";
     echo "    <td>" . $row['country'] . "<td>";
     echo "  </tr>";
   }

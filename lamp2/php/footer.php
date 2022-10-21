726f65422979382ac800ee20d5471ec02cb2b187
Jaden <spacemanjaden125@gmail.com>
2022-10-21 17:38:40 -0600

    Please stop tormenting me

--- lamp2/php/footer.php
+++ lamp2/php/footer.php
@@ -1,3 +1,9 @@
 <!-- begin footer -->
 
+<br><br>
+<div class = "mybuttonwrap">
+<button class = "btn btn-primary btn-lg mybutton" onclick="history.back()">&lt;&lt;Back</button>
+</div>
+<br>
+
 <p>The [INSERT FEATURES HERE] features of this webpage were all implemented using Bootstrap.</p>

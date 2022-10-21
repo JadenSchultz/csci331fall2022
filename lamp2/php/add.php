8cf4a07360b89f37da834fdcb9f8b5293d01e560
Jaden <spacemanjaden125@gmail.com>
2022-10-21 17:22:57 -0600

    Fixed button and footer things

--- lamp2/php/add.php
+++ lamp2/php/add.php
@@ -93,6 +93,12 @@ echo "<tbody>";
 echo "</tbody>";
 echo "</ttable>";
 
+<br><br>
+<div class = "mybuttonwrap">
+<button class = "btn btn-primary btn-lg mybutton" onclick="history.back()">&lt;&lt;Back</button>
+</div>
+<br>
+
 include "footer.php";
 
 } else {
@@ -108,11 +114,5 @@ $conn->close();
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
-
-<br><br>
-<div class = "mybuttonwrap">
-<button class = "btn btn-primary btn-lg mybutton" onclick="history.back()">&lt;&lt;Back</button>
-</div>
-<br>
 
 </html>
